ed5ec3fe4e6ac9f819a665b9efd196d42b319fff
Winson Cheng <83229800+0xKMG@users.noreply.github.com>
2024-06-02 23:57:59 +0800

    fix params for commitment amount

--- src/components/PendingEventsList/index.tsx
+++ src/components/PendingEventsList/index.tsx
@@ -53,11 +53,9 @@ export default function PendingEventsList() {
         });
 
         const { data } = await axios.get(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
-            Number(decodedCoordinates[0]) / 1000000
-          },${Number(decodedCoordinates[1]) / 1000000}&key=${
-            process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-          }`
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${Number(decodedCoordinates[0]) / 1000000},${
+            Number(decodedCoordinates[1]) / 1000000
+          }&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
         );
 
         evt.address = data.results[0].formatted_address;
@@ -65,43 +63,26 @@ export default function PendingEventsList() {
 
       console.log(data);
 
-      setEvents(
-        (data as any).sort(
-          (a: any, b: any) => Number(a.arrivalTime) - Number(b.arrivalTime)
-        )
-      );
+      setEvents((data as any).sort((a: any, b: any) => Number(a.arrivalTime) - Number(b.arrivalTime)));
     }
     getEvents();
   }, [walletClient]);
 
   return (
     <div className="">
-      <h2 className="text-4xl font-extrabold mb-5 text-black">
-        Pending Invites
-      </h2>
+      <h2 className="text-4xl font-extrabold mb-5 text-black">Pending Invites</h2>
       {/* MAP ALL INVITES */}
       {events.map((event: any, index: number) => (
         <div
           key={`pending_${index}`}
-          className="rounded-xl border-solid border-4 border-black text-left px-3 py-5 mb-3"
-        >
+          className="rounded-xl border-solid border-4 border-black text-left px-3 py-5 mb-3">
           <div className="flex items-center">
             <h3 className="font-bold text-2xl">{event.name}</h3>
-            <span className="ml-auto">
-              {format(
-                new Date(Number(event.arrivalTime)),
-                "yyyy-MM-dd (HH:mm)"
-              )}
-            </span>
+            <span className="ml-auto">{format(new Date(Number(event.arrivalTime)), "yyyy-MM-dd (HH:mm)")}</span>
           </div>
           <div>Location: {event.address}</div>
-          <div>
-            Commitment: $
-            {(Number(event.commitmentRequired) / 10 ** 18).toString()}
-          </div>
-          <div>
-            Penalty: ${(Number(event.penaltyRequired) / 10 ** 18).toString()}
-          </div>
+          <div>Commitment: ${(Number(event.commitmentRequired) / 10 ** 18).toString()}</div>
+          <div>Penalty: ${(Number(event.penaltyRequired) / 10 ** 18).toString()}</div>
           {/* <div className="mt-5">
             <span
               className={`bg-black text-white px-4 py-1 rounded-full mr-2 min-w-[100px]`}
@@ -117,30 +98,28 @@ export default function PendingEventsList() {
                   const account = await walletClient.getAddresses();
 
                   // approve spending
-                  const { request: approval } =
-                    await publicClient.simulateContract({
-                      address: "0xf8Bc58f8aef773aBBA1019E8aA048fc5AF876a38",
-                      abi: mockTokenAbi,
-                      functionName: "approve",
-                      args: [
-                        "0xadd81d4F68AB0420EdA840cFbc07Ff2d6fd708F1",
-                        parseEther(event.totalCommitment.toString()),
-                      ],
-                      account: account[0],
-                    });
+                  const { request: approval } = await publicClient.simulateContract({
+                    address: "0xf8Bc58f8aef773aBBA1019E8aA048fc5AF876a38",
+                    abi: mockTokenAbi,
+                    functionName: "approve",
+                    args: [
+                      "0xadd81d4F68AB0420EdA840cFbc07Ff2d6fd708F1",
+                      parseEther(event.commitmentRequired.toString()),
+                    ],
+                    account: account[0],
+                  });
                   await walletClient.writeContract(approval);
 
                   console.log("ready to accept", event.eventId);
 
                   // accept event
-                  const { request: accept } =
-                    await publicClient.simulateContract({
-                      address: "0xfcc5aff8946Aa3A8015959Bc468255489FcaD241",
-                      abi: abi,
-                      functionName: "acceptInvite",
-                      args: [event.eventId],
-                      account: account[0],
-                    });
+                  const { request: accept } = await publicClient.simulateContract({
+                    address: "0xfcc5aff8946Aa3A8015959Bc468255489FcaD241",
+                    abi: abi,
+                    functionName: "acceptInvite",
+                    args: [event.eventId],
+                    account: account[0],
+                  });
 
                   await walletClient.writeContract(accept);
 
@@ -153,8 +132,7 @@ export default function PendingEventsList() {
                   });
                 }
               }}
-              className="ml-auto bg-black text-white px-3 py-1 rounded-lg"
-            >
+              className="ml-auto bg-black text-white px-3 py-1 rounded-lg">
               ✅ Accept
             </button>
           </div>
